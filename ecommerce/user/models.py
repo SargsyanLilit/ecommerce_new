fd2a284ea5fd12f8e6bf262ef9464a9e1743ebe9
Lilit <sargsyanlilit1995@gmail.com>
2021-12-23 01:20:44 +0400

    updated the image and review username part

--- ecommerce/user/models.py
+++ ecommerce/user/models.py
@@ -13,7 +13,7 @@ class Profile(models.Model):
     address = models.CharField(max_length=100, null=True, blank=True)
     age = models.IntegerField(null=True, blank=True)
     gender = models.IntegerField(choices=STATUS_CHOICES, null=True, blank=True)
-    image = models.ImageField(default="products/images/profile_icon.png", upload_to=image_path, null=True, blank=True)
+    image = models.ImageField(default="default/profile_icon.png", upload_to=image_path, null=True, blank=True)
 
     # TODO discuss tp add user profile pic
 
